00b5affe1aa61b3061a9a4b8f62f7be7946c8390
Ricky Rivas <76985350+RickyRivas@users.noreply.github.com>
2021-12-04 19:11:34 -0600

    Update provide-vars.js

--- functions/provide-vars.js
+++ functions/provide-vars.js
@@ -1,10 +1,8 @@
 exports.handler = async () => {
-
+    const spaceId = process.env.ACCESS_TOKEN;
+    // const accessToken = process.env.SPACE_ID
     return {
         statusCode: 200,
-        body: JSON.stringify({
-            token: process.env.ACCESS_TOKEN,
-            space: process.env.SPACE_ID
-        })
+        body: JSON.stringify(spaceId)
     }
 }
