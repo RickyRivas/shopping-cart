eaecc2716db4347f557f3c6ae89918b908e398cb
Ricky Rivas <76985350+RickyRivas@users.noreply.github.com>
2021-12-05 16:47:16 -0600

    Update sendgrid-test.js

--- functions/sendgrid-test.js
+++ functions/sendgrid-test.js
@@ -2,7 +2,7 @@ const sgMail = require('@sendgrid/mail')
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 const msg = {
   to: `${process.env.FROM_EMAIL_ADDRESS}`, // Change to your recipient
-  from: `Rivas Web Designs Ecom Demo`, // Change to your verified sender
+  from: `${process.env.FROM_EMAIL_ADDRESS}`, // Change to your verified sender
   subject: 'Sending with SendGrid is Fun',
   text: 'and easy to do anywhere, even with Node.js',
   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
