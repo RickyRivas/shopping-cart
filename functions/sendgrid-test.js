d8f7162a107106dff27c1104d44918f915c6e62e
Ricky Rivas <76985350+RickyRivas@users.noreply.github.com>
2021-12-05 16:55:33 -0600

    Update sendgrid-test.js

--- functions/sendgrid-test.js
+++ functions/sendgrid-test.js
@@ -1,20 +1,26 @@
 // using Twilio SendGrid's v3 Node.js Library
 // https://github.com/sendgrid/sendgrid-nodejs
 
-const sgMail = require('@sendgrid/mail')
-sgMail.setApiKey('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')
-const msg = {
-  to: 'rickyrivas918@gmail.com', // Change to your recipient
-  from: 'rickyrivas918@gmail.com', // Change to your verified sender
-  subject: 'Sending with SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+exports.handler = async () => {
+    const sgMail = require('@sendgrid/mail')
+    sgMail.setApiKey('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')
+    const msg = {
+        to: 'rickyrivas918@gmail.com', // Change to your recipient
+        from: 'rickyrivas918@gmail.com', // Change to your verified sender
+        subject: 'Sending with SendGrid is Fun',
+        text: 'and easy to do anywhere, even with Node.js',
+        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    }
+    sgMail
+        .send(msg)
+        .then(() => {
+            console.log('Email sent')
+        })
+        .catch((error) => {
+            console.error(error)
+        })
+    return {
+        statusCode: 200,
+        body: JSON.stringify(msg)
+    }
 }
-sgMail
-  .send(msg)
-  .then(() => {
-    console.log('Email sent')
-  })
-  .catch((error) => {
-    console.error(error)
-  })
