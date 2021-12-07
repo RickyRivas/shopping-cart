f855904cd02d258b17cc77760c3004fef0b69167
Ricky Rivas <76985350+RickyRivas@users.noreply.github.com>
2021-12-07 14:04:03 -0600

    Update stripe-ele.js

--- functions/stripe-ele.js
+++ functions/stripe-ele.js
@@ -1,21 +1,20 @@
 var stripe = Stripe(STRIPE_SECRET_KEY, {
-  apiVersion: "2020-08-27",
+    apiVersion: "2020-08-27",
 });
 
+const domain = await stripe.applePayDomains.create({
+    domain_name: 'https://rwds1.netlify.app/',
+});
+const paymentIntent = await stripe.paymentIntents.create({
+    amount: 1099,
+    currency: 'usd'
+})
 exports.handler = async () => {
 
-    const domain = await stripe.applePayDomains.create({
-        domain_name: 'example.com',
-    });
-    const paymentIntent = await stripe.paymentIntents.create({
-        amount: 1099,
-        currency: 'usd'
-    })
-
     return {
-    statusCode: 200,
-    body: JSON.stringify({
-      publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-    })
-  }
+        statusCode: 200,
+        body: JSON.stringify({
+            publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+        })
+    }
 }
